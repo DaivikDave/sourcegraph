daa0b99c272d4d1e48ac926e300b84c2ddd19685
gitstart-sourcegraph <gitstart-sourcegraph@users.noreply.github.com>
2021-10-10 01:25:38 +0000

    feat: setup feature flag and UI for sign up optimization

--- client/web/src/auth/CloudSignUpPage.tsx
+++ client/web/src/auth/CloudSignUpPage.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames'
 import ChevronLeftIcon from 'mdi-react/ChevronLeftIcon'
-import GithubIcon from 'mdi-react/GithubIcon'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
@@ -8,13 +7,15 @@ import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryServi
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
 
 import { BrandLogo } from '../components/branding/BrandLogo'
+import { FeatureFlagProps } from '../featureFlags/featureFlags'
 import { SourcegraphContext } from '../jscontext'
 
 import styles from './CloudSignUpPage.module.scss'
-import { maybeAddPostSignUpRedirect } from './SignInSignUpCommon'
+import { OrDivider } from './OrDivider'
+import { ExternalsAuth } from './SignInSignUpCommon'
 import { SignUpArguments, SignUpForm } from './SignUpForm'
 
-interface Props extends ThemeProps, TelemetryProps {
+interface Props extends ThemeProps, TelemetryProps, FeatureFlagProps {
     source: string | null
     showEmailForm: boolean
     /** Called to perform the signup on the server. */
@@ -23,13 +24,13 @@ interface Props extends ThemeProps, TelemetryProps {
 }
 
 const SourceToTitleMap = {
-    Context: 'Easily search the code you care about.',
+    Context: 'Easily search the code you care about, for free',
     Saved: 'Create a library of useful searches.',
     Monitor: 'Monitor code for changes.',
     Extend: 'Augment code and workflows via extensions.',
-    SearchCTA: 'Easily search the code you care about.',
-    HomepageCTA: 'Easily search the code you care about.',
-    Snippet: 'Easily search the code you care about.',
+    SearchCTA: 'Easily search the code you care about, for free',
+    HomepageCTA: 'Easily search the code you care about, for free',
+    Snippet: 'Easily search the code you care about, for free',
 }
 
 export type CloudSignUpSource = keyof typeof SourceToTitleMap
@@ -46,8 +47,10 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
     onSignUp,
     context,
     telemetryService,
+    featureFlags,
 }) => {
     const location = useLocation()
+    const isSignupOptimised = featureFlags.get('signup-optimization')
 
     const queryWithUseEmailToggled = new URLSearchParams(location.search)
     if (showEmailForm) {
@@ -57,24 +60,12 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
     }
 
     const assetsRoot = window.context?.assetsRoot || ''
-
-    // Since this page is only intended for use on Sourcegraph.com, it's OK to hardcode
-    // GitHub and GitLab auth providers here as they are the only ones used on Sourcegraph.com.
-    // In the future if this page is intended for use in Sourcegraph Sever, this would need to be generalized
-    // for other auth providers such SAML, OpenID, Okta, Azure AD, etc.
-    const githubProvider = context.authProviders.find(provider =>
-        provider.authenticationURL?.startsWith('/.auth/github/login?pc=https%3A%2F%2Fgithub.com%2F')
-    )
-    const gitlabProvider = context.authProviders.find(provider =>
-        provider.authenticationURL?.startsWith('/.auth/gitlab/login?pc=https%3A%2F%2Fgitlab.com%2F')
-    )
-
     const sourceIsValid = source && Object.keys(SourceToTitleMap).includes(source)
     const title = sourceIsValid ? SourceToTitleMap[source as CloudSignUpSource] : SourceToTitleMap.Context // Use Context as default
 
-    const logEvent = (): void => {
+    const logEvent = (type: string): void => {
         if (sourceIsValid) {
-            telemetryService.log(`SignUpPLG${source || ''}_2_ClickedSignUp`)
+            telemetryService.log('SignupInitiated', { type }, { type })
         }
     }
 
@@ -121,26 +112,42 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
 
                 <div className={styles.signUpWrapper}>
                     <h2>Create a free account</h2>
-                    {!showEmailForm ? (
+                    {isSignupOptimised ? (
                         <>
-                            {githubProvider && (
-                                <a
-                                    href={maybeAddPostSignUpRedirect(githubProvider.authenticationURL)}
-                                    className={classNames(styles.signUpButton, styles.githubButton)}
-                                    onClick={logEvent}
-                                >
-                                    <GithubIcon className="mr-3" /> Continue with GitHub
-                                </a>
-                            )}
-                            {gitlabProvider && (
-                                <a
-                                    href={maybeAddPostSignUpRedirect(gitlabProvider.authenticationURL)}
-                                    className={classNames(styles.signUpButton, styles.gitlabButton)}
-                                    onClick={logEvent}
-                                >
-                                    <GitlabColorIcon className="mr-3" /> Continue with GitLab
-                                </a>
-                            )}
+                            <SignUpForm
+                                featureFlags={featureFlags}
+                                onSignUp={args => {
+                                    logEvent('form')
+                                    return onSignUp(args)
+                                }}
+                                context={{ authProviders: [], sourcegraphDotComMode: true }}
+                                buttonLabel="Sign up"
+                                experimental={true}
+                                className="my-3"
+                            />
+                            <div className={classNames('d-flex justify-content-center', styles.helperText)}>
+                                <span className="mr-1"> Have an account?</span>
+                                <Link to={`/sign-in${location.search}`}> Log in</Link>
+                            </div>
+
+                            <OrDivider className="mt-4 mb-4 text-lowercase" />
+
+                            <ExternalsAuth
+                                withCenteredText={true}
+                                context={context}
+                                githubLabel="Sign up with GitHub"
+                                gitlabLabel="Sign up with GitLab"
+                                onClick={logEvent}
+                            />
+                        </>
+                    ) : !showEmailForm ? (
+                        <>
+                            <ExternalsAuth
+                                context={context}
+                                githubLabel="Continue with GitHub"
+                                gitlabLabel="Continue with GitLab"
+                                onClick={logEvent}
+                            />
 
                             <div className="mb-4">
                                 Or,{' '}
@@ -162,8 +169,9 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
                             </small>
 
                             <SignUpForm
+                                featureFlags={featureFlags}
                                 onSignUp={args => {
-                                    logEvent()
+                                    logEvent('form')
                                     return onSignUp(args)
                                 }}
                                 context={{ authProviders: [], sourcegraphDotComMode: true }}
@@ -186,44 +194,17 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
                         .
                     </small>
 
-                    <hr className={styles.separator} />
+                    {!isSignupOptimised && (
+                        <>
+                            <hr className={styles.separator} />
 
-                    <div>
-                        Already have an account? <Link to={`/sign-in${location.search}`}>Log in</Link>
-                    </div>
+                            <div>
+                                Already have an account? <Link to={`/sign-in${location.search}`}>Log in</Link>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
     )
 }
-
-const GitlabColorIcon: React.FunctionComponent<{ className?: string }> = ({ className }) => (
-    <svg
-        className={className}
-        width="24"
-        height="24"
-        viewBox="-2 -2 26 26"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <path d="M9.99944 19.2025L13.684 7.86902H6.32031L9.99944 19.2025Z" fill="#E24329" />
-        <path
-            d="M1.1594 7.8689L0.037381 11.3121C-0.0641521 11.6248 0.0454967 11.9699 0.313487 12.1648L9.99935 19.2023L1.1594 7.8689Z"
-            fill="#FCA326"
-        />
-        <path
-            d="M1.15918 7.86873H6.31995L4.0989 1.04315C3.98522 0.693949 3.48982 0.693949 3.37206 1.04315L1.15918 7.86873Z"
-            fill="#E24329"
-        />
-        <path
-            d="M18.8444 7.8689L19.9624 11.3121C20.0639 11.6248 19.9542 11.9699 19.6862 12.1648L9.99902 19.2023L18.8444 7.8689Z"
-            fill="#FCA326"
-        />
-        <path
-            d="M18.8449 7.86873H13.6841L15.901 1.04315C16.0147 0.693949 16.5101 0.693949 16.6279 1.04315L18.8449 7.86873Z"
-            fill="#E24329"
-        />
-        <path d="M9.99902 19.2023L13.6835 7.8689H18.8444L9.99902 19.2023Z" fill="#FC6D26" />
-        <path d="M9.99907 19.2023L1.15918 7.8689H6.31995L9.99907 19.2023Z" fill="#FC6D26" />
-    </svg>
-)
--- client/web/src/auth/SignInSignUpCommon.tsx
+++ client/web/src/auth/SignInSignUpCommon.tsx
@@ -1,11 +1,34 @@
 import classNames from 'classnames'
 import * as H from 'history'
+import GithubIcon from 'mdi-react/GithubIcon'
 import * as React from 'react'
 
+import { LoaderInput } from '@sourcegraph/branded/src/components/LoaderInput'
+import { deriveInputClassName, InputValidationState } from '@sourcegraph/shared/src/util/useInputValidation'
+
+import { SourcegraphContext } from '../jscontext'
 import { USERNAME_MAX_LENGTH, VALID_USERNAME_REGEXP } from '../user'
+
+import styles from './CloudSignUpPage.module.scss'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     inputRef?: React.Ref<HTMLInputElement>
+}
+
+interface SignupEmailField {
+    emailState: InputValidationState
+    loading: boolean
+    label: string
+    nextEmailFieldChange: (changeEvent: React.ChangeEvent<HTMLInputElement>) => void
+    emailInputReference: React.Ref<HTMLInputElement>
+}
+
+interface ExternalsAuthProps {
+    context: Pick<SourcegraphContext, 'authProviders' | 'experimentalFeatures'>
+    githubLabel: string
+    gitlabLabel: string
+    onClick: (type: string) => void
+    withCenteredText?: boolean
 }
 
 export const PasswordInput: React.FunctionComponent<InputProps> = props => {
@@ -58,6 +81,123 @@ export const UsernameInput: React.FunctionComponent<InputProps> = props => {
             autoComplete="username"
             ref={inputRef}
         />
+    )
+}
+
+export const SignupEmailField: React.FunctionComponent<SignupEmailField> = ({
+    emailState,
+    loading,
+    label,
+    nextEmailFieldChange,
+    emailInputReference,
+}) => (
+    <div className="form-group d-flex flex-column align-content-start">
+        <label
+            htmlFor="email"
+            className={classNames('align-self-start', {
+                'text-danger font-weight-bold': emailState.kind === 'INVALID',
+            })}
+        >
+            {label}
+        </label>
+        <LoaderInput className={classNames(deriveInputClassName(emailState))} loading={emailState.kind === 'LOADING'}>
+            <EmailInput
+                className={deriveInputClassName(emailState)}
+                onChange={nextEmailFieldChange}
+                required={true}
+                value={emailState.value}
+                disabled={loading}
+                autoFocus={true}
+                placeholder=" "
+                inputRef={emailInputReference}
+            />
+        </LoaderInput>
+        {emailState.kind === 'INVALID' && <small className="invalid-feedback">{emailState.reason}</small>}
+    </div>
+)
+
+const GitlabColorIcon: React.FunctionComponent<{ className?: string }> = ({ className }) => (
+    <svg
+        className={className}
+        width="24"
+        height="24"
+        viewBox="-2 -2 26 26"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M9.99944 19.2025L13.684 7.86902H6.32031L9.99944 19.2025Z" fill="#E24329" />
+        <path
+            d="M1.1594 7.8689L0.037381 11.3121C-0.0641521 11.6248 0.0454967 11.9699 0.313487 12.1648L9.99935 19.2023L1.1594 7.8689Z"
+            fill="#FCA326"
+        />
+        <path
+            d="M1.15918 7.86873H6.31995L4.0989 1.04315C3.98522 0.693949 3.48982 0.693949 3.37206 1.04315L1.15918 7.86873Z"
+            fill="#E24329"
+        />
+        <path
+            d="M18.8444 7.8689L19.9624 11.3121C20.0639 11.6248 19.9542 11.9699 19.6862 12.1648L9.99902 19.2023L18.8444 7.8689Z"
+            fill="#FCA326"
+        />
+        <path
+            d="M18.8449 7.86873H13.6841L15.901 1.04315C16.0147 0.693949 16.5101 0.693949 16.6279 1.04315L18.8449 7.86873Z"
+            fill="#E24329"
+        />
+        <path d="M9.99902 19.2023L13.6835 7.8689H18.8444L9.99902 19.2023Z" fill="#FC6D26" />
+        <path d="M9.99907 19.2023L1.15918 7.8689H6.31995L9.99907 19.2023Z" fill="#FC6D26" />
+    </svg>
+)
+
+export const ExternalsAuth: React.FunctionComponent<ExternalsAuthProps> = ({
+    context,
+    githubLabel,
+    gitlabLabel,
+    onClick,
+    withCenteredText,
+}) => {
+    // Since this component is only intended for use on Sourcegraph.com, it's OK to hardcode
+    // GitHub and GitLab auth providers here as they are the only ones used on Sourcegraph.com.
+    // In the future if this page is intended for use in Sourcegraph Sever, this would need to be generalized
+    // for other auth providers such SAML, OpenID, Okta, Azure AD, etc.
+
+    const githubProvider = context.authProviders.find(provider =>
+        provider.authenticationURL?.startsWith('/.auth/github/login?pc=https%3A%2F%2Fgithub.com%2F')
+    )
+    const gitlabProvider = context.authProviders.find(provider =>
+        provider.authenticationURL?.startsWith('/.auth/gitlab/login?pc=https%3A%2F%2Fgitlab.com%2F')
+    )
+
+    return (
+        <>
+            {githubProvider && (
+                <a
+                    href={maybeAddPostSignUpRedirect(githubProvider.authenticationURL)}
+                    className={classNames(
+                        withCenteredText ? 'd-flex justify-content-center' : '',
+                        'text-decoration-none',
+                        styles.signUpButton,
+                        styles.githubButton
+                    )}
+                    onClick={() => onClick('github')}
+                >
+                    <GithubIcon className="mr-3" /> {githubLabel}
+                </a>
+            )}
+
+            {gitlabProvider && (
+                <a
+                    href={maybeAddPostSignUpRedirect(gitlabProvider.authenticationURL)}
+                    className={classNames(
+                        withCenteredText ? 'd-flex justify-content-center' : '',
+                        'text-decoration-none',
+                        styles.signUpButton,
+                        styles.gitlabButton
+                    )}
+                    onClick={() => onClick('gitlab')}
+                >
+                    <GitlabColorIcon className="mr-3" /> {gitlabLabel}
+                </a>
+            )}
+        </>
     )
 }
 
