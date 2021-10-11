ed9919a28a033600e9bdb04175dbad9d31eacf8d
Tom Ross <tom@umpox.com>
2021-10-11 09:45:15 +0100

    CI

--- dev/ci/yarn-lighthouse.sh
+++ dev/ci/yarn-lighthouse.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BASE_URL=http://localhost:3443
+BASE_URL=https://localhost:3443
 TEST_LABEL=$1
 TEST_PATH=$2
 
@@ -16,6 +16,12 @@ tar -xf client.tar.gz -C .
 echo "--- Yarn install in root"
 # mutex is necessary since CI runs various yarn installs in parallel
 NODE_ENV='' yarn --mutex network --frozen-lockfile --network-timeout 60000
+
+echo "--- Stopping Caddy"
+./dev/caddy.sh stop
+
+echo "--- Starting Caddy"
+./dev/caddy.sh start --config=dev/Caddyfile
 
 echo "--- Collecting Lighthouse results"
 yarn lhci collect --url="$BASE_URL$TEST_PATH"
